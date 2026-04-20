13da5f0ef38e3e1a3ce4b03d15fe2059405f3586
kunle <kush@KunleKush.localdomain>
2026-04-21 00:17:34 +0100

    fix: update eslint config to v9 format

---
+++ frontend/eslint.config.js
@@ -0,0 +1,18 @@
+const js = require("@eslint/js");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        require: "readonly",
+        process: "readonly",
+        console: "readonly",
+        module: "readonly",
+        __dirname: "readonly"
+      }
+    },
+    rules: {}
+  }
+];
